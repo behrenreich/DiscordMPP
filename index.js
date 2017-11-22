7bdd94d1f6bf034ea8a905fa314491bce6efdf37
Anon64 <30940003+Anon64@users.noreply.github.com>
2017-11-22 13:36:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ translate.key = process.env.YANDEX_TOKEN
 
 var bot = new Discord.Client();
 
-var lang = "ja";
+var lang = "en";
 
 var start = Math.round(Date.now() / 1000)
 
@@ -202,24 +202,6 @@ function name(name) {
 }
 var op = ["d55bf273f64f37c5691f3bbb"]
 var cmdChar = ">"
-setTimeout(function () {
-    var lob = new Client("ws://www.multiplayerpiano.com:8080");
-    var tyeet = new Client("ws://www.multiplayerpiano.com:8080");
-    tyeet.setChannel("test/yeet");
-    lob.setChannel("lolwutsecretlobbybackdoor");
-    lob.start();
-    tyeet.start();
-    lob.on("a", function (msg) {
-        if (msg.p._id == MPP.client.getOwnParticipant()._id) return;
-        dChat("381521631140380672", `**${msg.p.name.split("").join("\u034f")}** (\`${msg.p._id.substring(0, 4)}\`): ${msg.a}`)
-    })
-
-    tyeet.on("a", function (msg) {
-        if (msg.p._id == MPP.client.getOwnParticipant()._id) return;
-        dChat("382622516771946499", `**${msg.p.name.split("").join("\u034f")}** (\`${msg.p._id.substring(0, 4)}\`): ${msg.a}`)
-    })
-
-}, 3500)
 MPP.client.on("a", function (msg) {
     var isAdmin = false;
     var args = msg.a.split(' ');
@@ -278,6 +260,24 @@ bot.on("ready", () => {
     sendChat(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`, lang)
     bot.user.setGame(`on ${bot.guilds.size} servers`);
 })
+setTimeout(function () {
+    var lob = new Client("ws://www.multiplayerpiano.com:8080");
+    var tyeet = new Client("ws://www.multiplayerpiano.com:8080");
+    tyeet.setChannel("test/yeet");
+    lob.setChannel("lolwutsecretlobbybackdoor");
+    lob.start();
+    tyeet.start();
+    lob.on("a", function (msg) {
+        if (msg.p._id == MPP.client.getOwnParticipant()._id) return;
+        dChat("381521631140380672", `**${msg.p.name.split("").join("\u034f")}** (\`${msg.p._id.substring(0, 4)}\`): ${msg.a}`)
+    })
+
+    tyeet.on("a", function (msg) {
+        if (msg.p._id == MPP.client.getOwnParticipant()._id) return;
+        dChat("382622516771946499", `**${msg.p.name.split("").join("\u034f")}** (\`${msg.p._id.substring(0, 4)}\`): ${msg.a}`)
+    })
+
+}, 3500)
 bot.on("ready", () => {
     var dop = ["251985222915194881", "210605340201451521", "209015289990348800", "362315641161515008"]
     bot.on("message", function (message) {
