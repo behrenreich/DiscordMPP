922370a7ceb856e2368b46da82f1c2c37f7213b8
Anon64 <30940003+Anon64@users.noreply.github.com>
2017-11-27 21:59:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ function check(id) {
     return temp;
 }
 tried = false;
+a = false;
 math = function () {
     startingT = Date.now() / 1000
     tried = true;
@@ -114,11 +115,11 @@ math = function () {
     pts = randNum(15, 130);
     MPP.chat.send(`Math: what is ${rand} ${mathe} ${rand2}? >${pts} pts<`);
     MPP.client.on("a", function (m) {
-        if (m.a == ans) {
+        if (a) return;
+        if (m.a === ans) {
             if (!check(m.p._id)) {
                 people[m.p._id] = { pts: 0 };
             }
-            if (a) return;
             MPP.chat.send(`Math: correct! (that took you ${((Date.now() / 1000) - startingT).toFixed(3)} seconds)`);
             a = true;
             tried = false;
